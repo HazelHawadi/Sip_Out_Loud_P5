67718d2452c24323d349d7fef47fefb98ef46921
HazelHawadi <hawadihazel@icloud.com>
2025-03-01 22:17:39 +0000

    Add 'add to bag functionality'

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.shortcuts import redirect
 from .views import product_list
-from .views import product_list, product_management, search, add_to_cart, view_cart, update_cart, remove_from_cart
+from .views import product_list, product_management, search
 
 urlpatterns = [
     path('', product_list, name='products'),
@@ -13,9 +13,5 @@ urlpatterns = [
     path('products/<str:category>/', views.product_list, name='product_list_category'),
     path('<str:category>/', views.product_list, name='product_list_with_category'),  # Show category-specific products
     path('products/search/', views.search, name='search'),
-    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.view_cart, name='view_cart'),
-    path('update-cart/<int:product_id>/', views.update_cart, name='update_cart'),
-    path('remove-from-cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('manage/', views.product_management, name='product_management'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Q
-from .models import Product, CATEGORY_CHOICES
+from .models import Product, Category
 
 
 def product_list(request, category=None):
@@ -27,6 +27,57 @@ def product_list(request, category=None):
     })
 
 
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
+    }
+
+    return render(request, 'products/products.html', context)
+
 
 def product_management(request):
     return render(request, 'products/manage.html')
@@ -45,46 +96,4 @@ def search(request):
         'category': None  # Ensure category is handled properly
     })
 
-def add_to_cart(request, product_id):
-    product = Product.objects.get(id=product_id)
-
-    if 'cart' not in request.session:
-        request.session['cart'] = []
-
-    cart = request.session['cart']
-    cart.append({'id': product.id, 'name': product.name, 'price': product.price})
-
-    request.session['cart'] = cart
-    request.session.modified = True
-
-    return redirect('view_cart')
-
-def view_cart(request):
-    cart = request.session.get('cart', [])
-    return render(request, 'cart/cart.html', {'cart_items': cart})
-
-
-def update_cart(request, product_id):
-    if request.method == "POST":
-        quantity = int(request.POST.get('quantity', 1))
-        cart = request.session.get('cart', {})
-
-        if str(product_id) in cart:
-            cart[str(product_id)]['quantity'] = quantity
-
-        request.session['cart'] = cart
-        request.session.modified = True
-
-    return redirect('view_cart')
-
-def remove_from_cart(request, product_id):
-    cart = request.session.get('cart', {})
-
-    if str(product_id) in cart:
-        del cart[str(product_id)]
-
-    request.session['cart'] = cart
-    request.session.modified = True
-
-    return redirect('view_cart')
 
