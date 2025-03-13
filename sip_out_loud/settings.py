065206478710e7539cddaa42c42630806df8d46a
HazelHawadi <hawadihazel@icloud.com>
2025-03-13 12:27:24 +0000

    Deploying project

--- sip_out_loud/settings.py
+++ sip_out_loud/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d+2ti91)4=g$f*8x5tv7ph+ckuh)z1ivpo^h$eyk)*6+__4fx8'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
